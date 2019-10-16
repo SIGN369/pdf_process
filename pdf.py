2e694c325dc64976906c2daa89e9031d8356d67e
zyg <13375544370@qq.com>
2019-10-16 13:55:58 +0800

    d

--- pdf.py
+++ pdf.py
@@ -1,6 +1,6 @@
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter #导入需要的类（库）
-wp='F:/WECHAT/WeChat Files/QQ808782/FileStorage/File/2019-08/' #work_path
+wp='F:/WECHAT/WeChat Files/QQ808782/FileStorage/File/2019-06/' #work_path
 savePath='F:/WECHAT/WeChat Files/QQ808782/FileStorage/File/'#保存路径
 
 #合并同一个文件夹下的pdf文件
@@ -18,18 +18,24 @@ flst=[wp+f for f in flst]
 
 
 for pf in flst:
-    
-    out_pdf=PdfFileWriter()#每次重建新空对象
-    in_pdf=PdfFileReader(open(pf, 'rb')) #二进制打开
-    page_count=in_pdf.getNumPages() #获取pdf的页数，从0页开始
-    arr = pf.split("/");#截取文件名称
-    filename = arr[len(arr)-1];
-    print(filename)
-    for pc in range(page_count):
-        if pc!=1 and pc<(page_count-1):#删除第二页和最后一页
-            out_pdf.addPage(in_pdf.getPage(pc)) #逐页循环
-            with open(savePath+"temp/"+filename,'wb') as wf:
-                out_pdf.write(wf)
+    try:
+        out_pdf=PdfFileWriter()#每次重建新空对象
+        in_pdf=PdfFileReader(open(pf, 'rb')) #二进制打开
+        page_count=in_pdf.getNumPages() #获取pdf的页数，从0页开始
+        arr = pf.split("/");#截取文件名称
+        filename = arr[len(arr)-1];
+        print(filename)
+        for pc in range(page_count):
+            if pc!=1 and pc<(page_count-1):#删除第二页和最后一页
+                out_pdf.addPage(in_pdf.getPage(pc)) #逐页循环
+        with open(savePath+"temp/"+filename,'wb') as wf:
+            out_pdf.write(wf)
+            os.remove(pf)
+                    
+    except:
+        print("发生异常")
+    else:
+        print("OK")
 
 
 
